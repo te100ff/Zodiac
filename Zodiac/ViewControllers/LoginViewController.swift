1c6e0609939de22a5dfc1248844d34c3b2a58b29
Stanislav <te100ff@inbox.ru>
2021-04-23 18:17:31 +0300

    LoginVC start

---
+++ Zodiac/ViewControllers/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  Zodiac
+//
+//  Created by Stanislav Testov on 23.04.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var fieldLabels: [UILabel]!
+    
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var lastNameTF: UITextField!
+    @IBOutlet weak var birthdayTF: UITextField!
+    
+    @IBOutlet weak var okButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

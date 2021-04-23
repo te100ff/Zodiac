fdb9e78aa6565d69e2b8f8f2dd0218588317fc15
Bizblk <enemy_counter@mail.ru>
2021-04-23 12:16:15 +0300

    add model

---
+++ Zodiac/Models/DataManager.swift
@@ -0,0 +1,16 @@
+//
+//  DataManager.swift
+//  Zodiac
+//
+//  Created by Виталий Оранский on 23.04.2021.
+//
+
+import Foundation
+
+class DataManager {
+    
+    let names = ["Allan", "Damon", "Anthony", "Job", "Chester",
+                 "Marcia", "Felicia", "Andrea", "Gladys", "Julie"]
+    let surnames = ["Franklin", "Washington", "Thomas", "Austin", "Shelton",
+                   "Owen", "Kelley", "Stanley", "Fields", "Hunter"]
+}

ffd65866524996d386cf387913f10414c89398e4
Stanislav <te100ff@inbox.ru>
2021-04-24 19:04:30 +0300

    Some ZodiacVC setup

--- Zodiac/ViewControllers/ZodiacViewController.swift
+++ Zodiac/ViewControllers/ZodiacViewController.swift
@@ -18,10 +18,21 @@ class ZodiacViewController: UIViewController {
         super.viewDidLoad()
         zodiacLabel.text = "Вы \(person.zodiac.rawValue)"
         zodiacDescriptionTextView.text = person.zodiacDiscription
-        
+        assignbackground()
     }
     
-
+    func assignbackground(){
+        let background = UIImage(named: "stars")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
     
 
 }
